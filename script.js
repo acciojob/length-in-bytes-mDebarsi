7263259638bd7afad8f231e157735089757a6780
DEBARSI MONDAL <141905446+mDebarsi@users.noreply.github.com>
2023-12-08 20:32:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const byteSize = (str) => {
   // write your code here
+	// Create a Blob object with the string data
+  const blob = new Blob([str]);
+
+  // Get the size property of the Blob to get the byte size
+  const sizeInBytes = blob.size;
+
+  return sizeInBytes;
 };
 
 // Do not change the code below
